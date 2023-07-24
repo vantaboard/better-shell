7f88c2394c1482aada43db01c33fcb5f4635f965
Brighten Tompkins <brightenqtompkins@gmail.com>
2023-07-23 23:43:48 -0700

    init

---
+++ service-worker.ts
@@ -0,0 +1,13 @@
+function reddenPage() {
+    console.log('reddenPage');
+    document.body.style.backgroundColor = 'red';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    if (!tab.url.includes('chrome://')) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: reddenPage
+        });
+    }
+});
